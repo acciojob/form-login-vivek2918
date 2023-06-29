bc5d61ce0e17e27d6903b10dddbf5ead96312ee1
Vivek Chovatiya <119297870+vivek2918@users.noreply.github.com>
2023-06-29 18:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+ var form = document.getElementById('myForm');
 
+
+  var firstName = form.elements['firstName'].value;
+  var lastName = form.elements['lastName'].value;
+
+  
+  alert('First Name: ' + firstName + '\nLast Name: ' + lastName);
 }
